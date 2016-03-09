60cad1fbebc0b56c13a646769c74fc05612ac09f
igna92ts <igna_torres92@hotmail.com>
2016-03-09 02:46:58 -0300

    anda socket io

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
+//configuracion
+var config = require('./config')
 
 var app = express();
+app.locals = config // seteo la configuracion para que sea accesible mas tarde
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var sockets = require('./routes/socket');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +30,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/socket',sockets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
