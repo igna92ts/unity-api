8184d20ecca99822af58a5040cfa3c212fc9f91d
igna92ts <igna_torres92@hotmail.com>
2016-03-16 14:40:53 -0300

    No uso nohm

--- ServerDistribuido/SERVER1/app.js
+++ ServerDistribuido/SERVER1/app.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var io = require('socket.io')(http);
 var config = require('./config.js');
 
-//SETEO REDIS Y NOHM
+//SETEO REDIS 
 var redis = require('redis');
-var nohm = require('nohm').Nohm;
 var redisClient = redis.createClient(config.redis.port,config.redis.host);
-nohm.setClient(redisClient);
-var redisFunc = require('./redis_functions.js');
+
+//CONFIG REDIS Y TRAIGO PAQUETES
+var pkg_user = require('./redis_pkgs/pkg_user.js');
 redisClient.on('error',function(err){
     console.log("Error while connecting to redis" + err);
 });
@@ -19,12 +19,7 @@ redisClient.on('connect', function() {
 redisClient.on('diconnect', function() {
     console.log('disconnected from redis');
 });
-console.log(nohm);
-fs.readdirSync('./hash_models').forEach(function(file){
-  if(file.substr(-3) == '.js'){
-    nohm = require('./hash_models/' + file)(nohm);
-  }
-});
+
 
 //REGISTRO EL SERVICIO EN SEAPORT
 var seaport = require('seaport');
@@ -32,7 +27,7 @@ ports = seaport.connect(config.seaport.port,config.seaport.host);
 var counter = 0;
 
 io.on('connection', function(socket){
-  redisFunc.registerDynamicObject(nohm);
+  pkg_user.registerDynamicObject(redisClient);
 
   socket.on('disconnect',function(){
     console.log('user disconnected');
