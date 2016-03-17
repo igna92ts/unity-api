ee26b9a2c362afb99adaddef024f53c69abafaf7
igna92ts <igna_torres92@hotmail.com>
2016-03-17 01:29:39 -0300

    muchas cosas

--- ServerDistribuido/SERVER1/app.js
+++ ServerDistribuido/SERVER1/app.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var fs = require('fs');
 var io = require('socket.io')(http);
 var config = require('./config.js');
+var q = require('q');
 
 //SETEO REDIS 
 var redis = require('redis');
@@ -19,6 +20,11 @@ redisClient.on('connect', function() {
 redisClient.on('diconnect', function() {
     console.log('disconnected from redis');
 });
+//SET INDEXES
+redisClient.get('d_object_index',function(err,obj){
+  if(err || obj === null)
+    redisClient.set('d_object_index',0);
+});
 
 
 //REGISTRO EL SERVICIO EN SEAPORT
@@ -27,20 +33,27 @@ ports = seaport.connect(config.seaport.port,config.seaport.host);
 var counter = 0;
 
 io.on('connection', function(socket){
-  pkg_user.registerDynamicObject(redisClient);
 
   socket.on('disconnect',function(){
     console.log('user disconnected');
     console.log(counter);
   });
 
+  socket.on('NEW_PLAYER',function(){
+    //EL TIPO SIEMPRE EN MAYUSCULA LA PRIMERA
+    var player = pkg_user.registerDynamicObject(redisClient,'Player'); //DEVUELVE UNA PROMESA DEL RESULTADO DE LA QUERY
+    player.done(function(){
+      io.emit("OBJECT_CREATED",player.valueOf());
+    });
+  });
+
   socket.on('USER_CONNECT',function(msg){
-    console.log(msg);
     io.emit("USER_CONNECTED",msg);
   });
 
   socket.on('STATE_UPDATE',function(msg){
     counter ++;
+    
   });
 
 });
--- ServerDistribuido/SERVER1/redis_pkgs/pkg_user.js
+++ ServerDistribuido/SERVER1/redis_pkgs/pkg_user.js
@@ -1,36 +1,46 @@
 var redis = require('redis');
-var guid = require('guid');
+var q = require('q');
 
 
 module.exports = {
 
-	registerDynamicObject : function(client){
-		var objGUID = guid.create();
-		var hash = {
-			"GUID":objGUID,
-			"position":{
-				"x":0,
-				"y":0
-			},
-			"rotation":{
-				"x":0.0,
-				"y":0.0,
-				"z":0.0,
-				"w":1.0
-			},
-			"linearVelocity":{
-				"x":0.0,
-				"y":0.0,
-				"z":0.0
-			},
-			"angularVelocity":0.0
-		}
-
-		client.hmset('d_objects',hash,redis.print);
-	},
-
-	deleteDynamicObject : function(client){
-
+	registerDynamicObject: function(client,type){
+		return q.Promise(function(resolve,reject){
+			var hash = {
+				id:0,
+				type:type,
+				position:{
+					x:0,
+					y:0
+				},
+				"rotation":{
+					"x":0.0,
+					"y":0.0,
+					"z":0.0,
+					"w":1.0
+				},
+				"linearVelocity":{
+					"x":0.0,
+					"y":0.0,
+					"z":0.0
+				},
+				"angularVelocity":0.0
+			}
+			var objId;
+			client.incr('d_object_index',function(err,index){
+				hash.id = index;
+				client.hset('d_object',index,JSON.stringify(hash));
+				client.hgetall('d_object',function (err, obj) {
+					if(err){
+						reject(err);
+					}
+					else{
+						resolve(JSON.parse(obj[index]))
+					}
+				});
+			});
+		})
 	}
+
 
 }
